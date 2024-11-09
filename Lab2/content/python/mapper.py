ccf81e2649ce5ae53684364253b07da5a970c7ea
Andrei Conoval <andreiconoval@gmail.com>
2024-11-09 14:35:10 +0200

    Update lab2 script for python3

--- Lab2/content/python/mapper.py
+++ Lab2/content/python/mapper.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import re
 
 # read input from stdin, line by line
 for line in sys.stdin:
-  # strip trailing spaces and split into words
-  words = re.split('[\W]+', line.strip())
+    # strip trailing spaces and split into words
+    words = re.split(r'[\W]+', line.strip())
 
-  # emit <key, value> pairs
-  for word in words:
-    if word != '':
-      print '%s %s' % (word.lower(), 1)
+    # emit <key, value> pairs
+    for word in words:
+        if word != '':
+            print(f'{word.lower()} {1}')
--- Lab2/content/python/reducer.py
+++ Lab2/content/python/reducer.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
 d = {}
 
 for line in sys.stdin:
-  # strip and split considering only the first gap
-  word, count = line.strip().split(' ', 1)
+    # strip and split considering only the first gap
+    word, count = line.strip().split(' ', 1)
 
-  # convert the string to integer
-  try:
-    count = int(count)
-  except ValueError:
-    # if the value is not a number, discard line
-    continue
+    # convert the string to integer
+    try:
+        count = int(count)
+    except ValueError:
+        # if the value is not a number, discard line
+        continue
 
-  c = d.get(word)
-  c = 0 if c == None else c
-  d[word] = c + count
+    c = d.get(word)
+    c = 0 if c is None else c
+    d[word] = c + count
 
-for (k, v) in d.items():
-  print '%s %s' % (k, v)
+for k, v in d.items():
+    print(f'{k} {v}')
